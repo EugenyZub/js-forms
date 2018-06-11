683c263b2232590b380357fee44166ae027e045a
Eugene <evgeniys1@tut.by>
2018-06-11 16:23:57 +0300

    Strange email-checking

--- js/login-form-check.js
+++ js/login-form-check.js
@@ -7,43 +7,91 @@ $(document).ready(function(){
 			isValid: true,
 
 			init: function(){
-				// вызов внутренних функций
 				this._setUpListeners();
 			},
 
-			_setUpListeners: function(){
-				$('#comment_form').on('submit', formValidation._validateForm);
+			_setUpListeners: function() {
+				$('#enter-form').on('submit', formValidation._loginCheck);
 			},
 
-			_validateForm: function(event){
+			_loginCheck: function(event) {
 				event.preventDefault();
-				console.log('private method _validateForm - run');
+				console.log('private method _loginCheck - run');
 				var form = $(this),
-					inputs = form.find('input, textarea'),
+					email = form.find('input:text'),
+					password = form.find('input:password'),
 					valid = true;
-				
-				//метод .each - принимает 2 параметра: массив и функцию, окторую необходимо выполнить
-				$.each(inputs, function(index, val){
+
+				// Email
+				$.each(email, function(index, val){
+					console.log('поиск полей email');
 					var input = $(val),
-					 	value = input.val().trim(),
-					 	formGroup = input.parents('.form__group'),
-					 	//label = formGroup.find('label').text().toLowerCase(),
-					 	textError = 'Вы не ввели комментарий',
-					 	tooltip = $('<span class="form__tooltip">' + textError + '</span>');
-					//Проверям, что строка не пустая
-					if( value.length === 0 ) {
-						//Show errors
+						value = input.val().trim(),
+						formGroup = input.parents('.form__group'),
+						emailField = $('#emailField'),
+						pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+
+					if ( value.length === 0 ) {
+						// Show text error
+						formGroup.addClass('error');
+						tooltip = $('<span class="form__tooltip ">' + 'Пустой email' + '</span>');
+						formGroup.find('.form__tooltip').remove();
+						tooltip.appendTo(formGroup);
+						valid = false;
+						console.log('пустой email');
+					} else if( value.length != 0 ) {
+						if ( !(pattern.test(emailField.val())) ) {
+							// Show format error
+							formGroup.addClass('error');
+							tooltip = $('<span class="form__tooltip ">' + 'Неправильный формат email' + '</span>');
+							formGroup.find('.form__tooltip').remove();
+							tooltip.appendTo(formGroup);
+							valid = false;
+							console.log('неверный формат email');
+						}  else {
+							formGroup.removeClass('error');
+							formGroup.find('.form__tooltip').remove();
+							console.log('Удаление класса с ошибкой');
+						}
+					}
+
+
+
+					// Hide errors
+					input.on('focus', function(){
+						formGroup.find('.form__tooltip').remove();
+					});
+					input.on('keydown', function(){
+						formGroup.removeClass('error');
+					});
+					input.on('change', function(){
+						formGroup.removeClass('error');
+					});
+				}),
+
+				// Password 
+				$.each(password, function(index, val){
+					console.log('поиск полей с паролем');
+					var input = $(val),
+						value = input.val().trim(),
+						formGroup = input.parents('.form__group'),
+						textError = 'Вы не ввели пароль',
+						tooltip = $('<span class="form__tooltip ">' + textError + '</span>');
+						
+					if ( value.length === 0 ) {
+						// Show errors
 						formGroup.addClass('error');
 						formGroup.find('.form__tooltip').remove();
 						tooltip.appendTo(formGroup);
 						valid = false;
+						console.log('пустой пароль');
 					} else {
-						//Чтобы окно с ошибками было только одно
       					formGroup.removeClass('error');
       					formGroup.find('.form__tooltip').remove();
+						console.log('Удаление класса с ошибкой');
 					}
 
-					//Скрываем ошибки
+					// Hide errors
 					input.on('focus', function(){
 						formGroup.find('.form__tooltip').remove();
 					});
@@ -54,15 +102,9 @@ $(document).ready(function(){
 						formGroup.removeClass('error');
 					});
 				});
-
-				formValidation.isValid = valid;
-
-				if(valid){
-					window.location= "success.html";
-				}
 			},
-			
-		};
+
+		};	
 
 		formValidation.init();
 	}());
