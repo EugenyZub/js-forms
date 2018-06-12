3c3576179416736989b37f6b04722059600cbf5f
Eugene <evgeniys1@tut.by>
2018-06-12 14:37:48 +0300

    LoginCheck ready

--- js/login-form-check.js
+++ js/login-form-check.js
@@ -4,8 +4,6 @@ $(document).ready(function(){
 
 		var formValidation = {
 
-			isValid: true,
-
 			init: function(){
 				this._setUpListeners();
 			},
@@ -16,94 +14,104 @@ $(document).ready(function(){
 
 			_loginCheck: function(event) {
 				event.preventDefault();
-				console.log('private method _loginCheck - run');
+				//console.log('private method _loginCheck - run');
 				var form = $(this),
 					email = form.find('input:text'),
 					password = form.find('input:password'),
-					valid = true;
+					valid = true,
+					//для email
+					input1 = $(email),
+					value1 = input1.val().trim(),
+					formGroup1 = input1.parents('.form__group'),
+					emailField = $('#emailField'),
+					pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,6}\.)?[a-z]{2,6}$/i,
+					//для пароля
+					input2 = $(password),
+					value2 = input2.val().trim(),
+					formGroup2 = input2.parents('.form__group'),
+					//при вводе всего кроме нужных email пароля
+					formField = $('.form__error');
+					
 
 				// Email
 				$.each(email, function(index, val){
-					console.log('поиск полей email');
-					var input = $(val),
-						value = input.val().trim(),
-						formGroup = input.parents('.form__group'),
-						emailField = $('#emailField'),
-						pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-
-					if ( value.length === 0 ) {
+					//console.log('поиск полей email');
+						
+					if ( value1.length === 0 ) {
 						// Show text error
-						formGroup.addClass('error');
+						formGroup1.addClass('error');
 						tooltip = $('<span class="form__tooltip ">' + 'Пустой email' + '</span>');
-						formGroup.find('.form__tooltip').remove();
-						tooltip.appendTo(formGroup);
-						valid = false;
-						console.log('пустой email');
-					} else if( value.length != 0 ) {
-						if ( !(pattern.test(emailField.val())) ) {
+						formGroup1.find('.form__tooltip').remove();
+						tooltip.appendTo(formGroup1);
+						//console.log('пустой email');
+					} else if (!(pattern.test(value1)) && value1.length > 0 && value2.length > 0) {
 							// Show format error
-							formGroup.addClass('error');
+							formGroup1.addClass('error');
 							tooltip = $('<span class="form__tooltip ">' + 'Неправильный формат email' + '</span>');
-							formGroup.find('.form__tooltip').remove();
-							tooltip.appendTo(formGroup);
-							valid = false;
-							console.log('неверный формат email');
+							formGroup1.find('.form__tooltip').remove();
+							tooltip.appendTo(formGroup1);
+							//console.log('неверный формат email');							
 						}  else {
-							formGroup.removeClass('error');
-							formGroup.find('.form__tooltip').remove();
-							console.log('Удаление класса с ошибкой');
-						}
-					}
-
-
-
+							
+							formGroup1.removeClass('error');
+							formGroup1.find('.form__tooltip').remove();
+							//console.log('Удаление класса с ошибкой email');			
+							}
+					
 					// Hide errors
-					input.on('focus', function(){
-						formGroup.find('.form__tooltip').remove();
+					input1.on('focus', function(){
+						formGroup1.find('.form__tooltip').remove();
 					});
-					input.on('keydown', function(){
-						formGroup.removeClass('error');
+					input1.on('keydown', function(){
+						formGroup1.removeClass('error');
 					});
-					input.on('change', function(){
-						formGroup.removeClass('error');
+					input1.on('change', function(){
+						formGroup1.removeClass('error');
 					});
 				}),
 
 				// Password 
 				$.each(password, function(index, val){
-					console.log('поиск полей с паролем');
-					var input = $(val),
-						value = input.val().trim(),
-						formGroup = input.parents('.form__group'),
-						textError = 'Вы не ввели пароль',
-						tooltip = $('<span class="form__tooltip ">' + textError + '</span>');
+					//console.log('поиск полей с паролем');					
+					textError = 'Вы не ввели пароль',
+					tooltip = $('<span class="form__tooltip ">' + textError + '</span>');
 						
-					if ( value.length === 0 ) {
+					if ( value2.length === 0 ) {
 						// Show errors
-						formGroup.addClass('error');
-						formGroup.find('.form__tooltip').remove();
-						tooltip.appendTo(formGroup);
-						valid = false;
-						console.log('пустой пароль');
+						formGroup2.addClass('error');
+						formGroup2.find('.form__tooltip').remove();
+						tooltip.appendTo(formGroup2);
+						//console.log('пустой пароль');
 					} else {
-      					formGroup.removeClass('error');
-      					formGroup.find('.form__tooltip').remove();
-						console.log('Удаление класса с ошибкой');
+      					formGroup2.removeClass('error');
+      					formGroup2.find('.form__tooltip').remove();
+						//console.log('Удаление класса с ошибкой password');
 					}
 
 					// Hide errors
-					input.on('focus', function(){
-						formGroup.find('.form__tooltip').remove();
+					input2.on('focus', function(){
+						formGroup2.find('.form__tooltip').remove();
 					});
-					input.on('keydown', function(){
-						formGroup.removeClass('error');
+					input2.on('keydown', function(){
+						formGroup2.removeClass('error');
 					});
-					input.on('change', function(){
-						formGroup.removeClass('error');
+					input2.on('change', function(){
+						formGroup2.removeClass('error');
 					});
 				});
-			},
 
+				if ( (value1 == 'mail@mail.ru') && (value2 == '123') ) {
+					window.location= "success.html";
+				} else if (  ( (pattern.test(value1)) && value1.length > 0 && value2.length > 0 ) &&
+						  ( (value1 != 'mail@mail.ru') || (value2 != '123') )
+						) {
+								//console.log('Неверные email и/или пароль');
+								formField.addClass('error');
+								tooltip = $('<span class="form__tooltip ">' + 'Неверный email или пароль' + '</span>');
+								formField.find('.form__tooltip').remove();
+								tooltip.appendTo(formField);
+						}
+			},
 		};	
 
 		formValidation.init();
